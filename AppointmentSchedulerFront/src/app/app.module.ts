b8baeb1b6bc960c5602f3b4d2d59d27320139d6d
manoj <manoj@10.0.0.128>
2018-02-09 10:28:41 -0600

    login, nav bar, home and other page componenets created

--- AppointmentSchedulerFront/src/app/app.module.ts
+++ AppointmentSchedulerFront/src/app/app.module.ts
@@ -3,14 +3,33 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { RouterModule, Router } from '@angular/router';
+import { Routes } from '@angular/router/src/config';
+
+const appRoutes: Routes = [
+
+  { path:'', component: HomeComponent },
+  { path:'login', component: LoginComponent },
+  { path:'register', component: RegisterComponent }
+
+]
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
